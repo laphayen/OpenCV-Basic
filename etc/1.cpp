b34641baa956f5d5c65a90195dd1bd2931e44156
Baek Gi Chan <laphayen@gmail.com>
2022-08-01 23:02:18 +0900

    Upload etc1.cpp

---
+++ etc/1.cpp
@@ -0,0 +1,30 @@
+
+#include "opencv2/opecv.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+void video_in()
+{
+	VideoCapture cap("video name.avi");
+
+	if (!cap.isOpened()) {
+		cerr << "failed" << endl;
+		return;
+	} 
+
+	Mat frame, inversed;
+
+	while (true) {
+		cap >> frame;
+		if (frame.empty())
+			break;
+
+		imshow("inversed", inversed);
+
+ 		waitKey();
+ 	}
+ 	
+ 	destroyAllWindows();
+}
