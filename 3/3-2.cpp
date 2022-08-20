f5def70ca8e9eba6bbcfe4abc5b7c85758730b86
Baek Gi Chan <laphayen@gmail.com>
2022-08-20 16:08:14 +0900

    Update 3-2.cpp

--- 3/3-2.cpp
+++ 3/3-2.cpp
@@ -17,8 +17,13 @@ void SizeOp()
 	Size sz1, sz2;
 	sz1.width = 10; sz1.height = 20;
 	sz2(100, 200);
+	int wid1 = sz1.width();
+	int hei1 = sz1.height();
 	int area1 = sz1.area();
 
 	cout << "sz1: " << sz1 << endl;
 	cout << "sz2: " << sz2 << endl;
+	cout << "wid1: " << wid1 << endl;
+	cout << "hei1: " << hei1 << endl;
+	cout << "area1: " << area1 << endl;
 }
