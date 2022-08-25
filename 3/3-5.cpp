fcfef214d8aafc75dfa84661bde8c4736b669a76
Baek Gi Chan <laphayen@gmail.com>
2022-08-25 21:19:55 +0900

    Upload 3-5.cpp

---
+++ 3/3-5.cpp
@@ -0,0 +1,16 @@
+
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main() {
+	Vec<int, 3> X(1, 0, 0);
+	Vec<float, 3> Y(1, 0, 0);
+
+	cout<<"X"<<X<<endl;
+	cout<<"Y"<<Y<<endl;
+
+	return 0;
+}
