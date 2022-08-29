df54564c82cda165963f7ecaba717c3044913968
Baek Gi Chan <laphayen@gmail.com>
2022-08-29 23:59:22 +0900

    Upload 3-7.cpp

---
+++ 3/3-7.cpp
@@ -0,0 +1,21 @@
+
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+void MatOp();
+
+int main()
+{
+	MatOp();
+
+	return 0;
+}
+
+void MatOp()
+{
+	Mat img1(1280, 600, CV_8UC3);
+	
+}
