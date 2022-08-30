a16cbfb6a44aa936c9f486a8fda6c8477d232d9d
Baek Gi Chan <laphayen@gmail.com>
2022-08-30 23:52:43 +0900

    Upload 3-9.cpp

---
+++ 3/3-9.cpp
@@ -0,0 +1,13 @@
+
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main() {
+	
+	Mat img;
+	String filename = format("filename.bmp", img);
+	img = imread(filename);
+}
